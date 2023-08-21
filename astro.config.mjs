2f1b23272dabb231c4bd12d6bfe1a585879f3bf4
K Shrivathsa <68740255+vathsa-hebbar@users.noreply.github.com>
2023-08-21 19:51:25 +0000

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://tailviewer.vathsakabbinale.dev',
 	integrations: [tailwind(), sitemap()]
 });
