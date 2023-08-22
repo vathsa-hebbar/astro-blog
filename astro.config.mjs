cd66c1bada7b30d79582024bf28d2b8347dc72ec
K Shrivathsa <68740255+vathsa-hebbar@users.noreply.github.com>
2023-08-22 04:54:05 +0000

    chhanges

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tailviewer.vathsakabbinale.dev',
+	site: 'https://taleviewer.dev',
 	integrations: [tailwind(), sitemap()]
 });
