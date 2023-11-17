bebf534f64bfee8eb1d445c1562ade555ee50e30
K Shrivathsa <68740255+vathsa-hebbar@users.noreply.github.com>
2023-11-17 04:07:51 +0000

    changes

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://taleviewer.dev[',
+	site: 'https://taleviewer.dev',
 	integrations: [tailwind(), sitemap()]
 });
