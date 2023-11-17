7663657256a063adab30819d35465e4fb61f982e
K Shrivathsa <68740255+vathsa-hebbar@users.noreply.github.com>
2023-11-17 03:50:40 +0000

    changes

--- frontend-demo/astro.config.mjs
+++ frontend-demo/astro.config.mjs
@@ -1,8 +1,8 @@
-// my-blog/frontend/astro.config.mjs
-
 import { defineConfig } from 'astro/config';
+
 import sanity from "@sanity/astro";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [sanity({
     projectId: '55d7az5p',
