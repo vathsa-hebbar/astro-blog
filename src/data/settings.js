ef9abd37385c76d798a2b4abf3b4839499b0da4b
Shrivathsa Kabbinale <shrivathsakabbinale@Shrivathsas-MacBook-Air.local>
2024-01-23 23:41:14 +0530

    changes

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,7 +1,7 @@
 export default {
     title: `Taleviewer`,
     description: `This is an example blog made with Astro.`,
-    url: `https://taleviewer.dev`, // No trailing slash!
+    url: `http://taleviewer.dev`, // No trailing slash!
     // JSON LD
     name: `Shrivathsa Kabbinale`,
     // Facebook URL
